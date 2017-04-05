ece2e0612a87ef8a452e25f06e74961625db31ab
nielo22 <aramayo.fabian@gmail.com>
2017-04-05 16:02:31 -0300

    Subida de archivos.

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void altaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmProductos frm = new frmProductos();
+            frm.Show(this);
+        }
+
+        private void productoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
